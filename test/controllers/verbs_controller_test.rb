92e594e7a064648eb7eac0007589281f7061ff1f
lien <lienvandensteen@gmail.com>
2015-10-03 09:12:48 +0200

    prefer single quotes when no string interpolation or special symbols

--- test/controllers/verbs_controller_test.rb
+++ test/controllers/verbs_controller_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class VerbsControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
   end
 
-  test "should get create" do
+  test 'should get create' do
     post :create, verb: {dutch: 'spelen', english: 'play', ovt_me: 'speelde', ovt_you: 'speelden', vtt_verb: 'hebben', vtt_conjugated: 'gespeeld'}
   end
 
-  test "should get update" do
+  test 'should get update' do
     get :update, id: verbs(:eten).id
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     delete :destroy, id: verbs(:eten).id
     assert_response :success
   end
